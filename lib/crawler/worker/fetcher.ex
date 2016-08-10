12744d2254be02e53f458cb998db63d4c0861f9d
Fred Wu <ifredwu@gmail.com>
2016-08-10 18:47:57 +1000

    Refactored out `@fetch_opts`

--- lib/crawler/worker/fetcher.ex
+++ lib/crawler/worker/fetcher.ex
@@ -1,10 +1,15 @@
 defmodule Crawler.Worker.Fetcher do
+  @fetch_opts [
+    follow_redirect: true,
+    max_redirect:    5
+  ]
+
   def fetch(opts) do
     fetch_page(opts[:url])
   end
 
   def fetch_page(url) do
-    case HTTPoison.get(url, [], [follow_redirect: true, max_redirect: 5]) do
+    case HTTPoison.get(url, [], @fetch_opts) do
       {:ok, %{status_code: 200, body: body}} ->
         store_fetched_page(url, body)
       _ ->
