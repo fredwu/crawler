00b9f4e175f2d56ef277b83d1d8c4d36b9235641
Fred Wu <ifredwu@gmail.com>
2017-07-26 21:53:45 +1000

    Aliased `Crawler.Store.DB`

--- lib/crawler/store.ex
+++ lib/crawler/store.ex
@@ -1,20 +1,22 @@
 defmodule Crawler.Store do
+  alias Crawler.Store.DB
+
   defmodule Page do
     defstruct [:url, :body]
   end
 
   def init do
-    Registry.start_link(keys: :unique, name: Crawler.Store.DB)
+    Registry.start_link(keys: :unique, name: DB)
   end
 
   def add(url, body) do
-    Registry.register(Crawler.Store.DB, url, %Page{url: url, body: body})
+    Registry.register(DB, url, %Page{url: url, body: body})
 
     %Page{url: url, body: body}
   end
 
   def find(url) do
-    [{_, page}] = Registry.lookup(Crawler.Store.DB, url)
+    [{_, page}] = Registry.lookup(DB, url)
 
     page
   end
