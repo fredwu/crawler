69bba0b22e55d51a8646e4c757fac3f6646c51f4
Fred Wu <ifredwu@gmail.com>
2017-07-29 21:31:37 +1000

    Ensure `Registry.update_value` calls are successful

--- lib/crawler/store.ex
+++ lib/crawler/store.ex
@@ -30,12 +30,12 @@ defmodule Crawler.Store do
   end
 
   def add_body(url, body) do
-    Registry.update_value(DB, url, & %{&1 | body: body})
+    {_new, _old} = Registry.update_value(DB, url, & %{&1 | body: body})
 
     %Page{url: url, body: body}
   end
 
   def processed(url) do
-    Registry.update_value(DB, url, & %{&1 | processed: true})
+    {_new, _old} = Registry.update_value(DB, url, & %{&1 | processed: true})
   end
 end
