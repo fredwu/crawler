e8576d02bce6c9836e42b270a76cb6b4fc7246f0
Fred Wu <ifredwu@gmail.com>
2017-08-02 21:48:10 +1000

    Wipe the temp test directory only once

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,10 @@
 Code.require_file("test/helpers.ex")
 Code.require_file("test/test_case.ex")
 
+[File.cwd!, "test", "tmp", "*"]
+|> Path.join
+|> Path.wildcard
+|> Enum.each(&File.rm_rf/1)
+
 ExUnit.start()
 Application.ensure_all_started(:bypass)
