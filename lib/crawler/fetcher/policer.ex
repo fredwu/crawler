7d58538c127aff8d96bed79f2f0f4b9048cd3608
Fred Wu <ifredwu@gmail.com>
2017-08-20 16:52:09 +1000

    Only allow acceptable URI schemes

--- lib/crawler/fetcher/policer.ex
+++ lib/crawler/fetcher/policer.ex
@@ -6,21 +6,27 @@ defmodule Crawler.Fetcher.Policer do
 
   alias Crawler.Store
 
+  @uri_schemes ["http", "https"]
+
   @doc """
   ## Examples
 
       iex> Policer.police([depth: 1, max_depths: 2, url: "http://policer/"])
       {:ok, [depth: 1, max_depths: 2, url: "http://policer/"]}
 
+      iex> Policer.police([depth: 2, max_depths: 2])
+      {:error, "Fetch failed 'within_fetch_depth?', with opts: [depth: 2, max_depths: 2]."}
+
+      iex> Policer.police([depth: 1, max_depths: 2, url: "ftp://hello.world"])
+      {:error, "Fetch failed 'acceptable_uri_scheme?', with opts: [depth: 1, max_depths: 2, url: \\\"ftp://hello.world\\\"]."}
+
       iex> Crawler.Store.add("http://policer/exist/")
       iex> Policer.police([depth: 1, max_depths: 2, url: "http://policer/exist/"])
       {:error, "Fetch failed 'not_fetched_yet?', with opts: [depth: 1, max_depths: 2, url: \\\"http://policer/exist/\\\"]."}
-
-      iex> Policer.police([depth: 2, max_depths: 2])
-      {:error, "Fetch failed 'within_fetch_depth?', with opts: [depth: 2, max_depths: 2]."}
   """
   def police(opts) do
     with {_, true} <- within_fetch_depth?(opts),
+         {_, true} <- acceptable_uri_scheme?(opts),
          {_, true} <- not_fetched_yet?(opts)
     do
       {:ok, opts}
@@ -31,6 +37,14 @@ defmodule Crawler.Fetcher.Policer do
 
   defp within_fetch_depth?(opts) do
     {:within_fetch_depth?, opts[:depth] < opts[:max_depths]}
+  end
+
+  defp acceptable_uri_scheme?(opts) do
+    scheme = opts[:url]
+    |> String.split("://", parts: 2)
+    |> Kernel.hd
+
+    {:acceptable_uri_scheme?, Enum.member?(@uri_schemes, scheme)}
   end
 
   defp not_fetched_yet?(opts) do
