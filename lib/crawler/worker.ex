cc73d84a839a208ba955b8966904217c6c05dba9
Fred Wu <ifredwu@gmail.com>
2017-08-31 00:19:21 +1000

    Handle unprocessable messages
    (not sure why this is needed, as `GenServer` by default provides the exact same function when this callback is not implemented)

--- lib/crawler/worker.ex
+++ lib/crawler/worker.ex
@@ -31,6 +31,11 @@ defmodule Crawler.Worker do
     {:noreply, state}
   end
 
+  @doc false
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
   defp mark_processed({:ok, %Page{url: url}}), do: Store.processed(url)
   defp mark_processed(_),                      do: nil
 end
